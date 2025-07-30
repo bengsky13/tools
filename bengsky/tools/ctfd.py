df354fb17708fc3a9c3857b10a2fb5aa8cef3cf2
Bambang Priyanto <bengsky@Bambangs-MacBook-Air.local>
2025-07-30 17:23:42 +0700

    ctfd

--- bengsky/tools/ctfd.py
+++ bengsky/tools/ctfd.py
@@ -435,30 +435,3 @@ class Helper(object):
         for chunk in response.iter_content(512*1024):
           if chunk:
             f.write(chunk)
-def main():
-  parser = ArgumentParser(description='Simple CTFd-based scraper for challenges gathering')
-  parser.add_argument('--data', metavar='data', type=str, help='Populate from challs.json')
-  parser.add_argument('--proxy', metavar='proxy', type=str, help='Request behind proxy server')
-  parser.add_argument('--path', metavar='path', type=str, help='Target directory, default: CTF', default='CTF')
-  parser.add_argument('--worker',  metavar='worker', type=int, help='Number of threads, default: 10', default=10)
-  parser.add_argument('--scheme',  metavar='scheme', type=str, help='URL scheme, default: https', default='https')
-  parser.add_argument('--enable-cloud', help='Permit file download from a cloud drive, default=False', action='store_true')
-  parser.add_argument('--override', help='Override existed chall file', action='store_true')
-  parser.add_argument('--no-download', help='Don\'t download chall file', action='store_true')
-  parser.add_argument('--export', help='Export challenges directory as zip, default=False', action='store_true')
-  
-  args, unknown = parser.parse_known_args()
-  ctf  = CTFdScrape(args)
-  args.user = os.getenv('CTFD_USER')
-  args.passwd = os.getenv('CTFD_PASSWORD')
-  if args.data or args.url:
-    if args.user and args.passwd:
-      ctf.authenticate()
-      ctf.getChallenges()
-    else:
-      ctf.parseConfig(args.data)
-      ctf.nofile = True
-    ctf.createArchive()
-    ctf.review()
-  else:
-    parser.error('too few arguments')
