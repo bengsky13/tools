cba27e703696c9df0e4deb888f464bb87af4d67e
Bambang Priyanto <bengsky@Bambangs-MacBook-Air.local>
2026-05-08 18:13:08 +0700

    refactoor

--- bengsky/tools/ctfd.py
+++ bengsky/tools/ctfd.py
@@ -230,7 +230,7 @@ class CTFdScrape(object):
     while not q.empty():
       vals = self.chals[q.get()]
       ns   = Namespace(**vals)
-      print(ns, flush=True)
+      print(vals, flush=True)
       path = os.path.join(self.path, ns.category, ns.name)
       if not os.path.exists(path):
         os.makedirs(path)
