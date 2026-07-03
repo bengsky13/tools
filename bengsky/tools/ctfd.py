ea5baa1b3d60969a128c099e65694d8324358eef
Bambang Priyanto <bengsky@Bambangs-MacBook-Air.local>
2026-07-04 03:16:47 +0700

    refactoor

--- bengsky/tools/ctfd.py
+++ bengsky/tools/ctfd.py
@@ -88,6 +88,7 @@ class CTFdScrape(object):
   def __login(self):
     try:
       resp  = self.ses.get(self.lg_url)
+      print(resp.text)
       soup  = BeautifulSoup(resp.text,'lxml')
       nonce = soup.find('input', {'name':'nonce'}).get('value')
       self.auth['nonce'] = nonce
